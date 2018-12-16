99a9dd864c419e9303dc13e81a724e2412d7a8e6
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2018-12-15 22:19:14 -0500

    Add non-required path variable to GET /hello-world

--- src/main/java/com/example/todo/todo/controllers/HelloWorld.java
+++ src/main/java/com/example/todo/todo/controllers/HelloWorld.java
@@ -2,12 +2,19 @@ package com.example.todo.todo.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
-    @GetMapping("/hello-world")
-    public ResponseEntity<String> getHelloWorld() {
-        return ResponseEntity.ok("Hello World");
+    @GetMapping(value = {"/hello-world", "/hello-world/{customString}"})
+    public ResponseEntity<String> getHelloWorld(
+        @PathVariable(name = "customString", required = false) String customString
+    ) {
+        String responseString = "Hello, World!";
+        if (customString != null) {
+            responseString = customString;
+        }
+        return ResponseEntity.ok(responseString);
     }
 }
